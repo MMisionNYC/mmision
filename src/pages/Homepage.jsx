ccccf4912e26ad4a918264c3ef48ea295a4947b0
MMision <melissamision@gmail.com>
2025-06-07 11:12:11 -0500

    adding react helmet

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
 // import ButtonLoad from "../components/ButtonLoad";
 
 export default function Homepage() {
-	// const loadButton = () => {
-	// 	alert("Hello!");
-	// };
-
 	return (
+	<>
+      <Helmet>
+        <title>Home | Melissa Mision</title>
+        <meta name="description" content="Welcome to Melissa Mision's portfolio home page." />
+        <meta property="og:image" content="https://www.melissamision.com/homepage-background.jpg" />
+      </Helmet>
+      <h1>Home Page</h1>
 		<main className={styles.homepage}>
 			<PageNav />
 			<section>
@@ -27,5 +31,6 @@ export default function Homepage() {
 				</Link>
 			</section>
 		</main>
+			</>
 	);
 }
