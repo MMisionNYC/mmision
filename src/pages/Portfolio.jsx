587fc7f7582318f67ab81789c8edf0f61092e01f
MMisionNYC <mmisionnyc@gmail.com>
2025-10-21 14:32:33 -0500

    Update Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -34,7 +34,7 @@ Digital illustration has digital drawings/concepts and animations. Fine arts has
 						</div>
 					</div>
 					<div className={styles.bannerContainer}>
-						<Link reloadDocument to={"./src/pages/Development"}>
+						<Link reloadDocument to={"/Development"}>
 							<div className={styles.dev}>
 								<h2>Development</h2>
 							</div>
