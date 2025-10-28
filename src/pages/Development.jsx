bdb8f4e9e86112d50e5b188896bb13bd633ed076
MMisionNYC <mmisionnyc@gmail.com>
2025-10-28 14:42:18 -0500

    Update Development.jsx

--- src/pages/Development.jsx
+++ src/pages/Development.jsx
@@ -13,6 +13,14 @@ export default function Development() {
 				<meta name="description" content="Development projects by Melissa Mision" />
 				<meta property="og:image" content="https://www.melissamision.com/workAboutGrid.JPG" />
 			  </Helmet>
+						<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-WDNYYB8KSK"></script>
+<script>
+		window.dataLayer = window.dataLayer || [];
+ 		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+  		gtag('config', 'G-WDNYYB8KSK');
+</script>
 			<main className={styles.workPageGrid}>
 			<PageNav />
 			<div className={styles.workContainer}>
