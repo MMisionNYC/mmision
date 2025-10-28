07401eedd5a51e5c8881aa1bd9b6e15820d4b189
MMisionNYC <mmisionnyc@gmail.com>
2025-10-28 14:48:25 -0500

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -18,7 +18,7 @@ export default function Homepage() {
 		<script async src="https://www.googletagmanager.com/gtag/js?id=G-WDNYYB8KSK"></script>
 <script>
 		window.dataLayer = window.dataLayer || [];
- 		function gtag(){dataLayer.push(arguments);}
+ 		function gtag(){dataLayer.push(arguments)}
   		gtag('js', new Date());
   		gtag('config', 'G-WDNYYB8KSK');
 </script>
