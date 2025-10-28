d829261487fd3d21538df6a473508f5663c2abf3
MMisionNYC <mmisionnyc@gmail.com>
2025-10-28 10:05:33 -0500

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
+import Typing from "../components/Typing";
 // import ButtonLoad from "../components/ButtonLoad";
 
 export default function Homepage() {
