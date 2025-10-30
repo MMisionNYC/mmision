ba1e6c9763f520f95dc319420b2f9efdd3db2a69
MMisionNYC <mmisionnyc@gmail.com>
2025-10-30 17:25:17 -0500

    Update GalleryData.jsx

--- src/data/GalleryData.jsx
+++ src/data/GalleryData.jsx
@@ -22,14 +22,6 @@ if (window.location.pathname === "/DigitalIllustration") {
 			imagePath: "/illustration5.JPG",
 		},
 		{
-			name: "Airplane in Space",
-			description: "Digital illustration created with wacom tablet.",
-			bulletName: "Created with:",
-			bulletPoints: "Wacom tablet",
-			bulletPoints2: "Photoshop",
-			imagePath: "/illustration4.JPG",
-		},
-		{
 			name: "Aircraft Illustrations",
 			description: "Digital illustrations created with wacom tablet.",
 			bulletName: "Created with:",
