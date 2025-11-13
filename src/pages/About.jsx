5ef34d79ada6d825c3e14bacd169963f84465c8b
MMisionNYC <mmisionnyc@gmail.com>
2025-11-13 11:10:01 -0600

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -30,7 +30,7 @@ export default function About() {
 							<span className={styles.aboutPageh1Color}> Melissa.</span>
 						</h1>
 						<p>
-Creative front-end developer with 5+ years of experience bridging design and development. Skilled in JavaScript, React, and project lifecycle management. 
+Creative front-end developer with 5+ years of experience bridging design and development. Skilled in JavaScript, React, and project lifecycle management. Graduated from Flagler College in 2015 with a Bachelors of Fine Arts in Graphic Design and a minor in Illustration..
 <br></br>
 <br></br>
 Experienced in leading website migrations, improving UI responsiveness, and coordinating multi-stakeholder digital projects. Passionate about building accessible, user-focused digital experiences.
